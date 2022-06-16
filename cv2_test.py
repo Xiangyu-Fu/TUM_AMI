bd0f6c0b7982444e3f9dde0690050658195435fa
Stan Fu <2016mydream@gmail.com>
2022-06-16 10:25:28 +0200

    add new feature: save all the annotated images to a new folder

--- cv2_test.py
+++ cv2_test.py
@@ -1,9 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
+import os
 path = "logo_stan.png"
 
 im = cv2.imread(path)
 im_seg = im[0:100, 0:100, :]
+
+# save the image
+cv2.imwrite(os.path.join("test/logo_stan_seg.png"), im_seg)
+
 
 plt.imshow(im_seg)
 plt.show()
--- main.py
+++ main.py
@@ -8,49 +8,72 @@ import os
 import numpy as np
 import random
 
-TAG = "(main)"
-# Define the path to the dataset
-cocoRoot = "E:/AMI Project/Data"
 
-# Load COCO dataset
-annFile = os.path.join(cocoRoot, f'Annotations/annotated_functional_test3_fixed.json')
-print(TAG, f'Annotation file: {annFile}')
+# define the coco dataset process function
+def coco_dataset_process(save_images=False, save_annotations=False):
+    """
+    This function processes the coco dataset and saves the images and annotations.
+    :param save_images: default False. If true, the images will be saved.
+    :param save_annotations: default False. If true, the annotations will be saved.
+    :return: nothing.
+    """
+    TAG = "(coco_processing)"
+    # Define the path to the dataset
+    cocoRoot = "E:/AMI Project/Data/"
 
-# Load annotations
-coco = COCO(annFile)
+    # Load COCO dataset
+    annFile = os.path.join(cocoRoot, f'Annotations/annotated_functional_test3_fixed.json')
+    print(TAG, f'Annotation file: {annFile}')
 
-# load the ID of the category
-ids = coco.getCatIds('damage')
+    # Load annotations
+    coco = COCO(annFile)
 
-# Load image IDs
-imgIds = coco.getImgIds(catIds=ids)
-print(TAG, f'Number of images: {len(imgIds)}')
+    # load the ID of the category
+    ids = coco.getCatIds('damage')
 
-# get the information of the first image
-imgId = imgIds[0]
-imgInfo = coco.loadImgs(imgIds)[0]
-print(TAG, f'Image {imgId}\'s info: {imgInfo}')
+    # Load image IDs
+    imgIds = coco.getImgIds(catIds=ids)
+    print(TAG, f'Number of images: {len(imgIds)}')
 
-# show the image
-imPath = os.path.join(cocoRoot, 'Images', imgInfo['file_name'])
-im = cv2.imread(imPath)
-plt.imshow(im)
+    # save the annotated images
+    if save_images:
+        for imgId in imgIds:
+            # save the image
+            img = coco.loadImgs(imgId)[0]
+            file_name = img['file_name']
+            img_path = os.path.join(cocoRoot, f'Images/{file_name}')
+            img_name = os.path.basename(img_path)
+            img = cv2.imread(img_path)
+            cv2.imwrite(os.path.join(cocoRoot, f"Images/annotated/{img_name}"), img)
+    # TODO: save all the new annotations
+
+    # get the information of the first image
+    imgId = imgIds[0]
+    imgInfo = coco.loadImgs(imgIds)[0]
+    print(TAG, f'Image {imgId}\'s info: {imgInfo}')
+
+    # show the image
+    imPath = os.path.join(cocoRoot, 'Images', imgInfo['file_name'])
+    im = cv2.imread(imPath)
+    plt.imshow(im)
+
+    # get the damage area of the image
+    annIds = coco.getAnnIds(imgIds=imgInfo['id'])
+    anns = coco.loadAnns(annIds)
+    print(TAG, f'the annotation of the image: {anns}')
+
+    coco.showAnns(anns)
+    plt.show()
+
+    # show the image with the damage area
+    pos = anns[0]['segmentation'][0]
+    img_seg = im[pos[1]:pos[7], pos[0]:pos[2], :]
+
+    plt.imshow(img_seg)
+    plt.show()
 
 
-# get the damage area of the image
-annIds = coco.getAnnIds(imgIds=imgInfo['id'])
-anns = coco.loadAnns(annIds)
-print(TAG, f'the annotation of the image: {anns}')
+if __name__ == '__main__':
+    coco_dataset_process(save_images=True)
 
-coco.showAnns(anns)
-plt.show()
-
-
-# show the image with the damage area
-pos = anns[0]['segmentation'][0]
-print(pos[1], pos[7])
-img_seg = im[ pos[1]:pos[7], pos[0]:pos[2], :]
-
-plt.imshow(img_seg)
-plt.show()
 
